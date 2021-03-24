c24b0063ae9653eca80e2386f22519dc882f6f51
Ittachi_Uchiha <38896577+OmorFarukRakib@users.noreply.github.com>
2021-03-24 14:30:08 +0600

    Add files via upload

---
+++ js/rowDelete.js
@@ -0,0 +1,21 @@
+
+$("#csvRoot").on("click", ".deleteRow", function () {
+  // $("#csvRoot").DataTable().destroy();
+  // let table = $("#csvRoot").DataTable({
+  //   scrollY: 400,
+  //   scrollX: true,
+  //   scrollCollapse: true,
+  // });
+  var checkstr = confirm("Are you sure you want to delete this row?");
+
+  if (checkstr == true) {
+    
+    // do your code
+    //table.row($(this).parents("tr")).remove().draw();
+    $("#csvRoot").DataTable().row($(this).parents("tr")).remove().draw();
+  } else {
+    return false;
+  }
+});
+
+
---
+++ js/rowInsert.js
@@ -0,0 +1,28 @@
+$("#tableContainer").on("click", "#insert", function () {
+ 
+  var str = $("#insertNo").val();
+  console.log(str);
+
+   $("#csvRoot tbody tr:last")
+                .clone()
+                .appendTo("#dummy tbody")
+                .find(".table-data")
+                .empty();
+  var newRow = $("#dummy tbody tr:last").html();
+  document.getElementById("dummy").deleteRow(0);
+   
+  
+   //console.log( newRow);
+   if(str!=''){
+     let i;
+     for(i=0; i<str ; i++){
+        $("#csvRoot").DataTable().row.add($('<tr> ' + newRow + ' </tr>')).draw();
+     }
+   }else{
+    $("#csvRoot").DataTable().row.add($('<tr> ' + newRow + ' </tr>')).draw();
+   }
+  
+  // $("#csvRoot").DataTable().data.reload().draw();
+
+
+});
