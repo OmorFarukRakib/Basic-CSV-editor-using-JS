42880a7ffcfdb0ece8bd7042dc79eef6f386e307
Ittachi_Uchiha <38896577+OmorFarukRakib@users.noreply.github.com>
2021-03-24 14:29:19 +0600

    Delete rowInsert.js

--- js/rowInsert.js
+++
@@ -1,8 +0,0 @@
-$("#insert").click(function () {
-  console.log("clickedddddd");
-  $("#csvRoot tbody tr:last")
-    .clone()
-    .appendTo("#csvRoot tbody")
-    .find(".table-data")
-    .empty();
-});
