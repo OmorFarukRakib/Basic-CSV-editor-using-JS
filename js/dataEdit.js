651e6edb3a1bcb36703f144795db2441db3a267c
Ittachi_Uchiha <38896577+OmorFarukRakib@users.noreply.github.com>
2021-03-25 14:18:04 +0600

    Update dataEdit.js

--- js/dataEdit.js
+++ js/dataEdit.js
@@ -1,11 +1,42 @@
-function listenForDoubleClick(element) {
-    element.contentEditable = true;
-    setTimeout(function() {
-      if (document.activeElement !== element) {
-        element.contentEditable = false;
+// function listenForDoubleClick(element) {
+//     element.contentEditable = true;
+//     setTimeout(function() {
+//       if (document.activeElement !== element) {
+//         element.contentEditable = false;
+//       }
+//     }, 300);
+//   }
+
+
+//   $('#csvRoot').SetEditable({ $addButton: $('#addNewRow')});
+
+
+$(document).ready(function () {
+  $("#csvRoot").on("click", ".editbtn",function () {
+      var currentTD = $(this).parents('tr').find('td');
+      console.log(currentTD);
+      if ($(this).html() == 'Edit') {
+          currentTD = $(this).parents('tr').find('td');
+          $.each(currentTD, function () {
+              $(this).prop('contenteditable', true);
+              $(this).prop("className", "contentEditableStyle");
+              $('.editbtn').prop('contenteditable', false);
+              $('.deleteRow').prop('contenteditable', false);
+              $('.editbtn').closest( "td" ).removeClass('contentEditableStyle');
+              
+             
+              
+          });
+      } else {
+         $.each(currentTD, function () {
+              $(this).prop('contenteditable', false);
+              $(this).removeClass('contentEditableStyle');
+              
+          });
       }
-    }, 300);
-  }
 
+      $(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
 
-  
+  });
+
+});
