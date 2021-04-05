610849ea89cfe9d5ab000fe2c787678bbcbeb48b
Ittachi_Uchiha <38896577+OmorFarukRakib@users.noreply.github.com>
2021-04-05 14:00:50 +0600

    Update rowDelete.js

--- js/rowDelete.js
+++ js/rowDelete.js
@@ -1,4 +1,3 @@
-
 $("#csvRoot").on("click", ".deleteRow", function () {
   // $("#csvRoot").DataTable().destroy();
   // let table = $("#csvRoot").DataTable({
@@ -9,10 +8,32 @@ $("#csvRoot").on("click", ".deleteRow", function () {
   var checkstr = confirm("Are you sure you want to delete this row?");
 
   if (checkstr == true) {
-    
     // do your code
     //table.row($(this).parents("tr")).remove().draw();
     $("#csvRoot").DataTable().row($(this).parents("tr")).remove().draw();
+
+    // Needs to create new instance of datatable as table has been updated
+    $("#csvRoot").DataTable().destroy();
+    $("#csvRoot").DataTable({
+      destroy: true,
+      scrollY: 300,
+      scrollX: true,
+      scrollCollapse: true,
+      autoWidth: true,
+      responsive: true,
+    });
+
+    // Needs to re calculate the rows number and empty rows number Starts
+    let totalData = $("#csvRoot").DataTable().data().count();
+    let iColumns = $("#csvRoot thead th").length;
+    let totalRow = totalData / iColumns;
+
+    $("#TotalNumberOfTrackID").text(totalRow);
+    $("#TotalNumberOfEmptyTrackID").text(totalRow);
+
+    let totalEmptyRows = $("#csvRoot tr td:first-child:empty").length;
+    $("#TotalNumberOfEmptyTrackID").text(totalEmptyRows);
+    // Needs to re calculate the rows number and empty rows number Ends
   } else {
     return false;
   }
