598cdfa35bbb0431a42e3f741c84b675bf55e633
Ittachi_Uchiha <38896577+OmorFarukRakib@users.noreply.github.com>
2021-04-05 13:59:41 +0600

    Update dataEdit.js

--- js/dataEdit.js
+++ js/dataEdit.js
@@ -7,36 +7,57 @@
 //     }, 300);
 //   }
 
-
 //   $('#csvRoot').SetEditable({ $addButton: $('#addNewRow')});
 
-
 $(document).ready(function () {
-  $("#csvRoot").on("click", ".editbtn",function () {
-      var currentTD = $(this).parents('tr').find('td');
-      console.log(currentTD);
-      if ($(this).html() == 'Edit') {
-          currentTD = $(this).parents('tr').find('td');
-          $.each(currentTD, function () {
-              $(this).prop('contenteditable', true);
-              $(this).prop("className", "contentEditableStyle");
-              $('.editbtn').prop('contenteditable', false);
-              $('.deleteRow').prop('contenteditable', false);
-              $('.editbtn').closest( "td" ).removeClass('contentEditableStyle');
-              
-             
-              
-          });
-      } else {
-         $.each(currentTD, function () {
-              $(this).prop('contenteditable', false);
-              $(this).removeClass('contentEditableStyle');
-              
-          });
-      }
+  $("#csvRoot").on("click", ".editbtn", function () {
+    var currentTD = $(this).parents("tr").find("td");
+    console.log("Ata crnt ID " + currentTD);
+    if ($(this).html() == "Edit") {
+      currentTD = $(this).parents("tr").find("td");
+      $.each(currentTD, function () {
+        $(this).prop("contenteditable", true);
+        $(this).prop("className", "contentEditableStyle");
+        $(".editbtn").prop("contenteditable", false);
+        $(".deleteRow").prop("contenteditable", false);
+        $(".editbtn").closest("td").removeClass("contentEditableStyle");
+        $(".deleteRow").closest("td").removeClass("table-data");
+      });
+    } else {
+      $.each(currentTD, function () {
+        $(this).prop("contenteditable", false);
+        $(this).removeClass("contentEditableStyle");
+        $(this).prop("className", "table-data");
+        $(".deleteRow")
+          .closest("td")
+          .removeClass(["table-data", "contentEditableStyle"]);
+        // Needs to create new instance of datatable as table has been updated
+        $("#csvRoot").DataTable().destroy();
+        $("#csvRoot").DataTable({
+          destroy: true,
+          scrollY: 300,
+          scrollX: true,
+          scrollCollapse: true,
+          autoWidth: true,
+          responsive: true,
+        });
+      });
+    }
+    $(".editbtn").closest("td").removeClass("table-data");
+    // Needs to re calculate the rows number and empty rows number Starts
+    let totalData = $("#csvRoot").DataTable().data().count();
+    let iColumns = $("#csvRoot thead th").length;
+    let totalRow = totalData / iColumns;
 
-      $(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
+    $("#TotalNumberOfTrackID").text(totalRow);
+    $("#TotalNumberOfEmptyTrackID").text(totalRow);
 
+    let totalEmptyRows = $("#csvRoot tr td:first-child:empty").length;
+    $("#TotalNumberOfEmptyTrackID").text(totalEmptyRows);
+    // Needs to re calculate the rows number and empty rows number Ends
+
+    $(this).html($(this).html() == "Edit" ? "Save" : "Edit");
   });
-
+  $(".editbtn").closest("td").removeClass("table-data");
+  $("#csvRoot tbody tr td:last").removeClass("table-data");
 });
